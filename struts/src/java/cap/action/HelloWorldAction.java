5357f0eda8c5900e2b2feca113644e7d11a3147d
Romantic <286543570@qq.com>
2018-09-07 21:02:12 +0800

    第一次Struts1的事例。

--- struts/src/java/cap/action/HelloWorldAction.java
+++ struts/src/java/cap/action/HelloWorldAction.java
@@ -2,19 +2,19 @@ package cap.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class HelloWorldAction extends ActionSupport {
-    private String username;
+        public class HelloWorldAction extends ActionSupport {
+            private String username;
 
-    public String getUsername() {
-        return username;
-    }
+            public String getUsername() {
+                return username;
+            }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+            public void setUsername(String username) {
+                this.username = username;
+            }
 
-    public String sayHi() {
+            public String sayHi() {
 
-        return SUCCESS;
+                return SUCCESS;
     }
 }
